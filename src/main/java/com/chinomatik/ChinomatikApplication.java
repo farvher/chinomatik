efd32def2407454f324552db061abc04d76ae32c
farvher <fesanmiguel@misena.edu.co>
2017-10-18 11:33:01 -0500

    primer commit

---
+++ src/main/java/com/chinomatik/ChinomatikApplication.java
@@ -0,0 +1,12 @@
+package com.chinomatik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinomatikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChinomatikApplication.class, args);
+	}
+}
